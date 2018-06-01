46d38046f8a2f1ec204261d1bb55e2b1330ed35a
andrewmegaris <andrew.megaris@gmail.com>
2018-06-01 14:27:20 -0500

    unlimited looping powers!

--- examples/scan_and_print.cpp
+++ examples/scan_and_print.cpp
@@ -12,7 +12,7 @@ int main(int argc, char **argv)
   my_radar_test.activate();
   std::cout << "radar activate finished" << std::endl;
   my_radar_test.enable_wide_mode();
-  for(int x = 0; x < 10; x++)
+  while(true)
   {
     my_radar_test.get_scan();
     my_radar_test.print_scan_info();
